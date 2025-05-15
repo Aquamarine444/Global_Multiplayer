bbf3dfb42e14bfdc9903755a01eb92b44d87c78d
Aquamarine444 <miatharmse@gmail.com>
2025-05-15 16:12:28 +0200

    Lighting

--- Test_Global_Multiplayer/Assets/Scripts/FPS.cs
+++ Test_Global_Multiplayer/Assets/Scripts/FPS.cs
@@ -30,6 +30,11 @@ public class FPSPlayer : NetworkBehaviour //NetworkBehaviour - class that comes/
         if (!isLocalPlayer) //if not this computer's player, disable camera
         {
             playerCamera.gameObject.SetActive(false);
+            gameObject.GetComponent<PlayerInput>().enabled = false;
+        }
+        else
+        {
+            gameObject.GetComponent<PlayerInput>().enabled = true;
         }
 
         // Lock cursor
